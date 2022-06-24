29b36dc48a62d2c0da0e271054e0c7ed128d1dc5
sirsemy <hajdu1szabolcs@gmail.com>
2022-06-24 18:15:52 +0200

    Create due date calculator route

---
+++ app/Http/Controllers/DateCalculateController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DateCalculateController extends Controller
+{
+    /**
+     * Calculate due date from submitted date and turnaround time.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function CalculateDueDate(Request $request)
+    {
+        //
+    }
+}
